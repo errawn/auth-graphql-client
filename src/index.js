f2c65e8fc009e0b189e421170375522c04d1f140
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-06-23 00:41:15 +0800

    Apollo Provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import App from './App'
+import Header from './components/Header'
+import Profile from './components/Profile'
+
+const networkInterface = createNetworkInterface({
+  uri: '/graphql'
+});
+
+const client = new ApolloClient({
+	networkInterface,
+	dataIdFromObject: o => o.id
+})
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+  		  <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/profile" component={Profile} />
+      	</Switch>
+      </Router>
+    </ApolloProvider>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
