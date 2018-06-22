d1d5fd63cb042b235b9c5253ed350605be2110db
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-06-23 01:09:17 +0800

    get token

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React from 'react'
-import ApolloClient from 'apollo-client'
-import { ApolloProvider } from 'react-apollo'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
-const client = new ApolloClient({
-  // enable caching thru id
-  dataIdFromObject: o => o.id
-})
+class App extends React.Component {
+	constructor(props) {
+		super(props)
 
-const App = () => {
-  return (
-    <ApolloProvider client={client}>
-      <div>
-        Hello World!
-      </div>
-    </ApolloProvider>
-  )
+		this.state = { email: 'edrren@gmail.com', password: 'password' }
+	}
+
+	onSubmit = async (e) => {
+		e.preventDefault()
+		const { email, password } = this.state
+
+		const response = await this.props.mutate({
+			variables: { email, password }
+		})
+
+		const { data: { signin: { token } } } = response
+		console.log(token)
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.onSubmit.bind(this)}>
+				<label>Email</label>
+				<input type="text" onChange={(e) => this.setState({ email: e.target.value})} value={this.state.email} />
+				<br/>
+				<label>Password</label>
+				<input type="password" onChange={(e) => this.setState({ password: e.target.value})} value={this.state.password} />
+				<button type="submit" >Login</button>
+			</form>
+		)
+	}
+
 }
 
-export default App
+const mutation = gql`
+	mutation SignIn($email: String!, $password: String!){
+	  signin(email: $email, password: $password) {
+	    token
+	  }
+	}
+`;
+
+export default graphql(mutation)(App)
