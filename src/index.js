e776abb4ed12a4b998a53a24cf62a457cc7ff90e
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-06-23 15:50:10 +0800

    Add /home to routes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import App from './App'
 import Header from './components/Header'
+import Home from './components/Home'
 import Login from './components/Login'
 import Profile from './components/Profile'
 
@@ -27,7 +28,8 @@ const Root = () => {
           <Header />
           <App>
             <Switch>
-              <Route exact path="/" component={Login} />
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
               <Route path="/profile" component={Profile} />
             </Switch>
           </App>
