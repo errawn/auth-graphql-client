40507d91e8063b7ba80bf9fd042e3f3345d7084a
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-06-23 16:48:39 +0800

    Add success redirection, error alert message checkAuth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { Redirect } from 'react-router-dom'
 
 import {  
+	Alert,
 	Button,
 	ControlLabel, 
 	Form,  
@@ -10,27 +12,50 @@ import {
 	FormGroup,
 } from 'react-bootstrap'
 
+import { checkAuth } from '../services/auth'
+
 class Login extends React.Component {
 	constructor(props) {
 		super(props)
 
-		this.state = { email: 'edrren@gmail.com', password: 'password' }
+		this.state = { 
+			email: 'edrren@gmail.com', 
+			password: 'password',
+			errorMsg: false
+		}
 	}
 
 	onSubmit = async (e) => {
 		e.preventDefault()
 		const { email, password } = this.state
 
-		const response = await this.props.mutate({
-			variables: { email, password }
-		})
+		try {
+			const response = await this.props.mutate({
+				variables: { email, password }
+			})
+			const { data: { signin: { token } } } = response
+			this.setState({ errorMsg: false })
+			localStorage.setItem('token', token)
 
-		const { data: { signin: { token } } } = response
-		console.log(token)
+			this.props.history.push('/profile')
+		} catch(e) {
+			this.setState({ errorMsg: true })
+			localStorage.removeItem('token')
+		}
 	}
 
 	render() {
+		if (checkAuth())
+			return <Redirect to="/profile" />
+
 		return (
+			<div>
+			{ this.state.errorMsg ? (
+				<Alert bsStyle="danger" onDismiss={this.handleDismiss}>
+		          <h4>Oh snap! You got an error!</h4>
+		          <p>Login Failed. Please provide correct credentials.</p>
+	        	</Alert>
+			): null}
 			<Form onSubmit={this.onSubmit.bind(this)}>
 				<FormGroup
 					role="form"
@@ -56,6 +81,7 @@ class Login extends React.Component {
 				</FormGroup>
 				<Button type="submit">Sign In</Button>
 			</Form>
+			</div>
 		)
 	}
 
