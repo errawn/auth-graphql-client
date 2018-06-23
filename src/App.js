c2e45e73d3a4e9b13fb1192a28c009efae467da8
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-06-23 15:35:07 +0800

    Separated App to Login Component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,12 @@
-import React from 'react'
-import gql from 'graphql-tag'
-import { graphql } from 'react-apollo'
+import React, { Component } from 'react'
 
-class App extends React.Component {
-	constructor(props) {
-		super(props)
 
-		this.state = { email: 'edrren@gmail.com', password: 'password' }
-	}
-
-	onSubmit = async (e) => {
-		e.preventDefault()
-		const { email, password } = this.state
-
-		const response = await this.props.mutate({
-			variables: { email, password }
-		})
-
-		const { data: { signin: { token } } } = response
-		console.log(token)
-	}
-
-	render() {
-		return (
-			<form onSubmit={this.onSubmit.bind(this)}>
-				<label>Email</label>
-				<input type="text" onChange={(e) => this.setState({ email: e.target.value})} value={this.state.email} />
-				<br/>
-				<label>Password</label>
-				<input type="password" onChange={(e) => this.setState({ password: e.target.value})} value={this.state.password} />
-				<button type="submit" >Login</button>
-			</form>
-		)
-	}
-
+const App = ({ children }) => {
+	return (
+		<div className="container">
+			{children}
+		</div>
+	)
 }
 
-const mutation = gql`
-	mutation SignIn($email: String!, $password: String!){
-	  signin(email: $email, password: $password) {
-	    token
-	  }
-	}
-`;
-
-export default graphql(mutation)(App)
+export default App
