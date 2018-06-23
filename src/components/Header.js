b53c859fdf504c39b6b6d2d8d2d6c587a0a9b7ff
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-06-23 16:49:27 +0800

    Implemented checkauth in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Nav, MenuItem, Navbar, NavItem, NavDropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
+import { checkAuth } from '../services/auth'
+
 class Header extends Component {
   render() {
     return (
@@ -12,22 +14,29 @@ class Header extends Component {
           </Navbar.Brand>
         </Navbar.Header>
         <Nav>
-          <NavItem>
-            <Link to="/login">Login</Link>
-          </NavItem>
-          <NavItem>
-            <Link to="/signup">Sign Up</Link>
-          </NavItem>
-          <NavDropdown eventKey={3} title="Profile" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>
-              <Link to="/profile">My Profile</Link>
-            </MenuItem>
-            <MenuItem eventKey={3.2}>
-              <Link to="/blogs">My Blogs</Link>
-            </MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={3.4}>Logout</MenuItem>
-          </NavDropdown>
+          { !checkAuth() ? (
+            <NavItem>
+              <Link to="/login">Login</Link>
+            </NavItem>) : null
+          }
+          { !checkAuth() ? (
+            <NavItem>
+              <Link to="/signup">Sign Up</Link>
+            </NavItem>
+          ) : null }
+         
+          { checkAuth() ? (
+            <NavDropdown eventKey={3} title="Profile" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>
+                <Link to="/profile">My Profile</Link>
+              </MenuItem>
+              <MenuItem eventKey={3.2}>
+                <Link to="/blogs">My Blogs</Link>
+              </MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.4}>Logout</MenuItem>
+            </NavDropdown>
+          ) : null }
         </Nav>
       </Navbar>
     )
