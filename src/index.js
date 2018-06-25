306cff46166d885ddec76e6fde4152308ac459bb
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-06-25 10:09:14 +0800

    Update Auth Header Apollo config

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import ApolloClient from 'apollo-boost'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
 import { ApolloProvider } from 'react-apollo'
 
 import Routes from './routes'
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: '/graphql',
-  headers: {
-    "authentication": localStorage.getItem('token') || null
-  }
-});
+})
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authentication: token || null
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 const Root = () => {
   return (
