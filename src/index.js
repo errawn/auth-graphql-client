2c50b8e4598e6945c0738812f0b67b30a60a288e
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-06-26 15:53:51 +0800

    exported apollo client

--- src/index.js
+++ src/index.js
@@ -1,39 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { setContext } from 'apollo-link-context'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-
 import { ApolloProvider } from 'react-apollo'
 
 import Routes from './routes'
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-})
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authentication: token || null
-    }
-  }
-})
-
-const cache = new InMemoryCache({
-  dataIdFromObject: object => object.id || null
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache
-});
+import client from './client'
 
 const Root = () => {
   return (
