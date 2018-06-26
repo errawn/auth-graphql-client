f977c9ffb154f824d37aa6ebf36e37898fc79402
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-06-26 14:08:29 +0800

    Fetch Single Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Query } from 'react-apollo'
+
+import FETCH_POST from '../queries/post_query'
 
 class Post extends Component {
 	render() {
 		return (
-			<div>
-				<Link to='/posts'>Back</Link>
-				<h1>Post with id: {this.props.computedMatch.params.id}</h1>
-				<p>post body</p>
-			</div>
+			<Query 
+				query={FETCH_POST}
+				variables={ { id: this.props.computedMatch.params.id } }
+			>
+			{({ loading, error, data }) => {
+				if (loading) return <p>Loading...</p>
+				if (error) return <p>Something went wrong</p>
+				return (
+					<div>
+						<Link to='/posts'>Back</Link>
+						<h1>{data.post.title}</h1>
+						<p>{data.post.body}</p>
+					</div>
+				)
+			}}
+			</Query>
 		)
 	}
 }
